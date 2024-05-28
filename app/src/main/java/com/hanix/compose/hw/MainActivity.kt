4de9ed934af71acfef56883cc471f00c875fca93
hanix_x <hanix_x@paycrux.co.kr>
2024-05-28 11:55:42 +0900

    Chapter 1 / 열과 행 만들기

--- app/src/main/java/com/hanix/compose/hw/MainActivity.kt
+++ app/src/main/java/com/hanix/compose/hw/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hanix.compose.hw.ui.theme.Hw_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,48 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Hw_ComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("Hani", "Compose\nHappy")) {
+    Column(modifier = modifier) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = {}) {
+                Text(text = "Click Me !")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     Hw_ComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
