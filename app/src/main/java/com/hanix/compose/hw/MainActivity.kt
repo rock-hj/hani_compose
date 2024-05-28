52b69b65e2250bff54504f1a6774030c204344fc
hanix_x <hanix_x@paycrux.co.kr>
2024-05-28 13:51:01 +0900

    Chapter 1 / Compose 에서의 상태 - visibility

--- app/src/main/java/com/hanix/compose/hw/MainActivity.kt
+++ app/src/main/java/com/hanix/compose/hw/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,24 +45,38 @@ fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("Hani", "C
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(24.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .padding(24.dp)
         ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Hello ")
-                Text(text = name)
+            Row(
+
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    Text(text = "Hello ")
+                    Text(text = name)
+                }
+                ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                    Text(text = if (expanded.value) "Show less" else "Show more")
+                }
             }
-            ElevatedButton(onClick = {}) {
-                Text(text = "Click Me !")
+            if (expanded.value) {
+                Text(text = "more text", modifier = Modifier.padding(vertical = 20.dp))
             }
         }
+
     }
 }
 
