75ef4aee97ce3ac8af7cec4d29c50c0d833531b0
hanix_x <hanix_x@paycrux.co.kr>
2024-05-29 15:51:12 +0900

    Chapter 1 / 앱 스타일 & 테마 설정

--- Chapter_1/CodeLab_1/app/src/main/java/com/hanix/compose/hw/MainActivity.kt
+++ Chapter_1/CodeLab_1/app/src/main/java/com/hanix/compose/hw/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hanix.compose.hw
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -27,6 +28,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hanix.compose.hw.ui.theme.Hw_ComposeTheme
@@ -97,7 +99,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                         .padding(bottom = extraPadding.coerceAtLeast(0.dp))
                 ) {
                     Text(text = "Hello ")
-                    Text(text = name)
+                    Text(text = name, style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.ExtraBold))
                 }
                 ElevatedButton(onClick = { expanded = !expanded }) {
                     Text(text = if (expanded) "Show less" else "Show more")
@@ -136,6 +138,14 @@ fun GreetingPreview() {
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "GreetingDark")
+@Composable
+fun GreetingDarkPreview() {
+    Hw_ComposeTheme {
+        Greeting("Hani")
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
@@ -144,7 +154,7 @@ fun OnboardingPreview() {
     }
 }
 
-@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun MyAppPreview() {
     Hw_ComposeTheme {
