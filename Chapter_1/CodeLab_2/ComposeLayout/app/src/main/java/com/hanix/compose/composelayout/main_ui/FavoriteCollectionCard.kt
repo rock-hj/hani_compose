b714c2d35c0fd26e6af63d15b8588f846194a81b
hanix_x <ixxwxfjlwf@gmail.com>
2024-05-30 11:53:36 +0900

    Chapter 1 / Card

---
+++ Chapter_1/CodeLab_2/ComposeLayout/app/src/main/java/com/hanix/compose/composelayout/main_ui/FavoriteCollectionCard.kt
@@ -0,0 +1,65 @@
+package com.hanix.compose.composelayout.main_ui
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hanix.compose.composelayout.R
+import com.hanix.compose.composelayout.ui.theme.ComposeLayoutTheme
+
+@Composable
+fun FavoriteCollectionCard(
+    @DrawableRes image: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        onClick = { },
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(255.dp),
+        ) {
+            Image(
+                painter = painterResource(image),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun FavoriteCollectionCardPreview() {
+    ComposeLayoutTheme {
+        FavoriteCollectionCard(
+            image = R.drawable.fc2_nature_meditations,
+            text = R.string.fc2_nature_meditations,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
