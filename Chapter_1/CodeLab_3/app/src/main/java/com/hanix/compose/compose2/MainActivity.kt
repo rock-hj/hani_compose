9c30e4d60976b6097048ca558e024a6a82069367
hanix_x <ixxwxfjlwf@gmail.com>
2024-05-30 09:46:18 +0900

    Chapter 1 / 기본 레이아웃 init

---
+++ Chapter_1/CodeLab_3/app/src/main/java/com/hanix/compose/compose2/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.hanix.compose.compose2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hanix.compose.compose2.main_ui.WellnessScreen
+import com.hanix.compose.compose2.ui.theme.Compose2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Compose2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Compose2Theme {
+        WellnessScreen()
+    }
+}
