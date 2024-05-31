a4edcef6bfef66781f6e5c430dde0214bc515756
hanix_x <ixxwxfjlwf@gmail.com>
2024-05-31 11:42:18 +0900

    Chapter 1 / Codelab 3

--- Chapter_1/CodeLab_3/app/src/main/java/com/hanix/compose/compose2/MainActivity.kt
+++ Chapter_1/CodeLab_3/app/src/main/java/com/hanix/compose/compose2/MainActivity.kt
@@ -1,19 +1,40 @@
 package com.hanix.compose.compose2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.hanix.compose.compose2.main_ui.WellnessScreen
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hanix.compose.compose2.ui.theme.Compose2Theme
 
 class MainActivity : ComponentActivity() {
@@ -33,18 +54,191 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+class WellnessViewModel : ViewModel() {
+
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) =
+        _tasks.find { it.id == item.id }?.let { task ->
+            task.checked = checked
+        }
+}
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun WellnessTaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = taskName,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+        Checkbox(checked = checked, onCheckedChange = onCheckedChange)
+        IconButton(onClick = { onClose() }) {
+            Icon(imageVector = Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+
+@Composable
+fun WellnessTasksItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue ->
+            Log.d("tag", "checkedChange = $newValue")
+            checkedState = newValue
+        },
+        onClose = {}, // we will implement this later!
+        modifier = modifier,
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WellnessTaskItemPreview() {
+    Compose2Theme {
+        WellnessTasksItem(taskName = "This is a task")
+    }
+}
+
+class WellnessTask(val id: Int, val label: String, checked: Boolean = false) {
+    var checked by mutableStateOf(checked)
+}
+
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() },
+    onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit
+) {
+    LazyColumn(modifier = modifier) {
+        items(list, key = { task -> task.id }) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                onClose = { onCloseTask(task) },
+                checked = task.checked,
+                onCheckedChange = {checked -> onCheckedTask(task,checked)}
+            )
+        }
+    }
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableIntStateOf(0) }
+        if (count > 0) {
+            var showTask by rememberSaveable { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    checked = true,
+                    onCheckedChange = {},
+                    onClose = { showTask = false },
+                )
+            }
+            Text(
+                text = stringResource(R.string.count_glass, count),
+                modifier = modifier.padding(16.dp)
+            )
+        }
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaterCounterPreview() {
+    Compose2Theme {
+        WaterCounter()
+    }
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableIntStateOf(0) }
+
+        StatelessCounter(count, { count++ }, modifier)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StatefulCounterPreview() {
+    Compose2Theme {
+        StatefulCounter()
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun WellnessPreview() {
     Compose2Theme {
         WellnessScreen()
     }
+}
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCloseTask = { task -> wellnessViewModel.remove(task) },
+            onCheckedTask = {task , checked ->
+                wellnessViewModel.changeTaskChecked(task,checked)
+            }
+        )
+    }
+
 }
--- Chapter_1/CodeLab_3/app/src/main/java/com/hanix/compose/compose2/main_ui/WellnessScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.hanix.compose.compose2.main_ui
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
-}
